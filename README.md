dc087c165220298ee51b756f7706a07bf1ba2d0e
impyadav <pyadav223141@gmail.com>
2022-01-12 13:21:02 +0530

    file updated for smooth pip run

--- README.md
+++ README.md
@@ -1,46 +1,41 @@
 
 ![PyPI - License](https://img.shields.io/pypi/l/QA-Annotator) ![PyPI](https://img.shields.io/pypi/v/QA-Annotator)
 
-<h1 style="font-family: 'Roboto Mono', monospace;text-align align="center">QA-Annotator: Annotation Is All You Need</h1>
+# QA-Annotator: Annotation Is All You Need
 
 
-<!-- ABOUT THE PROJECT -->
 ## About The Project
 
 Making the mammoth task of data annotation for an NLP-based Question-Answering system hassle-free. Just jump right in and start annotating!
 
-
-<!-- GETTING STARTED -->
 ## Getting Started
  
 ### Installation
 
 Install with pip (python 3 required)
    ```sh
-   $ pip install QA-Annotator 
+   $ pip install qa-annotator 
    ```
 ### Start the server
    ```sh
-   $ annotator 
+   $ annotator run 
    ```
-<!-- CONTRIBUTING -->
 ## Contributing
 
 Contributions, suggestions are welcomed! :)
 
 If you like the project, Don't forget to give a star! Thanks!
 
-<!-- LICENSE -->
 ## License
 
 Distributed under the MIT License. See `LICENSE.txt` for more information.
 
 
-<!-- CONTACT -->
-<!--- ## Contact
+## Contact
 
-1. Prateek Yadav - [@impyadav_](https://twitter.com/impyadav_)
+Prateek Yadav - [@impyadav_](https://twitter.com/impyadav_)
 
---->
+## Links
+
 Project Link: [https://pypi.org/project/QA-Annotator/](https://pypi.org/project/QA-Annotator/)
 
--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md', 'r') as f:
+    README = f.read()
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='QA-Annotator',
-    version='1.0.0',
-    packages=['annotator'],
+    name='qa_annotator',
+    version='1.0.0.dev9',
+    packages=find_packages(),
+    # include_package_data=True,
+    package_data={
+        'annotator': ['.env', '*.sqlite3', 'templates/*', 'static/**/*', '*.txt', '*.md', '.flaskenv'],
+        'static': ['**/*.css', '**/*.map', '**/*.gif', '**/*.js'],
+        'templates': ['*.html'],
+    },
+    entry_points={
+        'console_scripts': [
+            'annotator=annotator.app:main'
+        ]},
     url='https://github.com/impyadav/QA-Annotator',
-    download_url='https://github.com/impyadav/QA-Annotator/archive/refs/tags/v_1.0.0.tar.gz',
+    download_url='https://github.com/impyadav/QA-Annotator/archive/refs/tags/v_1.1.0.tar.gz',
     install_requires=required,
     license='MIT',
     author='Praveen Singh, Shubham Modi, Prateek Yadav',
     author_email='impyadav.tech@gmail.com',
-    description='Data Annotation Tool for NLP-based Question-Answering systems',
-    long_description='Making the mammoth task of data annotation for an NLP-based Question-Answering system hassle-free. Just jump right in and start annotating! 🤞'
-
+    description='Data Annotation Tool for NLP-based Question-Answering systems.',
+    long_description_content_type='text/markdown',
+    long_description=README
 )
